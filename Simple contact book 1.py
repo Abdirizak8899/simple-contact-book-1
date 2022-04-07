2c0f61e82f53e9b45f7f60ee69dc373453516b8f
Abdirizak8899 <102373033+Abdirizak8899@users.noreply.github.com>
2022-04-07 10:54:31 +0300

    Add files via upload

---
+++ Simple contact book 1.py
@@ -0,0 +1,46 @@
+# simple contact book 
+print('SIMPLE CONTACT BOOK')
+print()
+print('1.Add contact')
+
+User = input()
+
+if User == '1':
+	print()
+	name = input('Enter contact name: ')
+	phone = input('Enter contact phone number: ')
+	print()
+	print('Name = ', name)
+	print('Phone = ', phone)
+
+Con = False
+
+while Con == False:
+	print('SIMPLE CONTACT BOOK')
+	print()
+	print('1.Add contact')
+	print('2.search contact')
+	print('0.Exit')
+	User = input()
+	if User == '1':
+		print()
+		name = input('Enter contact name: ')
+		phone = input('Enter contact phone number: ')
+		name.strip()
+		phone.strip()
+		print()
+		print('Name = ', name)
+		print('Phone = ', phone)
+		
+	elif User == '2':
+			sear = input('Enter contact number or name: ')
+			if sear == name or sear == phone:
+				print('Name:', name)
+				print('Phone: ', phone)
+			else:
+				print('No contact found!')
+	elif User == '0':
+		Con = True
+	else:
+		continue
+		
